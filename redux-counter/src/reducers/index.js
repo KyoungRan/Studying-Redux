b2391ea6256e9ffdf935a7732897e2197518aa4d
KyoungRan K <kyoungrankang@gmail.com>
2018-05-02 18:38:59 +0900

    added immutable with multi counter

--- redux-counter/src/reducers/index.js
+++ redux-counter/src/reducers/index.js
@@ -49,6 +49,7 @@ function counter(state = initialState, action) {
 ** }
 ** 로 만들어집니다.
 */
+/*
 import * as types from '../actions/ActionTypes';
 
 // 초기 상태를 정의합니다. 
@@ -123,5 +124,54 @@ function counter(state = initialState, action) {
       return state;
   }
 };
+*/
+import { Map, List } from 'immutable';
+import * as types from '../actions/ActionTypes';
 
+// 초기 상태를 정의합니다. 
+const initialState = Map({
+  counters: List([
+    Map({
+      color: 'black',
+      number: 0
+    })
+  ])
+})
+
+// 리듀서 함수를 정의합니다. 
+function counter(state = initialState, action) {
+  const counters = state.get('counters');
+
+  switch(action.type) {
+    // 카운터를 새로 추가합니다.
+    case types.CREATE:
+      return state.set('counters', counters.push(Map({
+        color: action.color,
+        number: 0
+      })))
+    // slice 를 이용하여 맨 마지막 카운터를 제외시킵니다. 
+    case types.REMOVE:
+      return state.set('counters', counters.pop());
+    // action.index 번째 카운터의 number 에 1을 더 합니다.
+    case types.INCREMENT:
+      return state.set('counters', counters.update(
+        action.index,
+        (counter) => counter.set('number', counter.get('number') + 1))
+      );
+    // action.index 번째 카운터의 number에 1을 뺍니다.
+    case types.DECREMENT:
+      return state.set('counters', counters.update(
+        action.index,
+        (counter) => counter.set('number', counter.get('number') - 1))
+    );
+    // action.index 번째 카운터의 색상을 변경합니다. 
+    case types.SET_COLOR:
+      return state.set('counters', counters.update(
+        action.index,
+        (counter) => counter.set('color', action.color))
+      );
+    default:
+      return state;
+  }
+}
 export default counter;
